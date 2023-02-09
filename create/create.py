7702e8b252664563cca635eec5beac1152c449ac
= <mateuspfeffer@gmail.com>
2023-02-09 09:52:53 -0300

    Created class to generate all necessary keys

--- create/create.py
+++ create/create.py
@@ -10,14 +10,27 @@ class Create():
         temp = dict()
         temp["title"] = data["name"]
         temp["description"] = data["description"]
+        temp["termsOfService"] = "http://link-to-your-tos.com/fell-free-to-delete"
+        temp["contact"] = {"email": "youremail@feelsafetodelete.com"}
+        temp["license"] = {
+            "name": "License name, you can delete the 'license' parent",
+            "url": "http://license-url.com/"
+        }
         temp["version"] = "1.0.0"
+
 
         return temp
 
-    
+
     def createServers(self, data: dict) -> list:
         temp = dict()
         l = list()
+
+        for i in data:
+            if (str(i["value"]).startswith("http")):
+                temp["url"] = i["value"]
+
+                l.append(temp)
 
         return l
     
@@ -26,7 +39,7 @@ class Create():
         l = list()
         temp = dict()
 
-        for i in data["item"]:
+        for i in data:
             temp["name"] = i["name"]
             try:
                 temp["description"] = str(i["description"]).split(".")[0]
@@ -40,7 +53,6 @@ class Create():
 
     def createParameters(self, data: dict) -> list:
         param = list()
-        temp = dict()
 
         for i in data["request"]["url"]["path"]:
             if ("{{" in i): # check if there's a parameter in the url path
@@ -58,7 +70,10 @@ class Create():
         if (method == "get"):
             return None
 
-        return Populate.populateRequestBody(data["header"][0]["value"])
+        try:
+            return Populate.populateRequestBody(data["header"][0]["value"])
+        except:
+            return None
     
 
     def createResponseBody(self, data: dict, method: str) -> dict:
@@ -68,56 +83,55 @@ class Create():
             return Populate.populateResponseBody(data[0])
         elif (len(data) > 1):
             for i in data:
-                if ("code" in i.keys()):
-                    temp[str(i["code"])] = dict()
-                    temp[str(i["code"])]["description"] = data[0]["status"]
-
-                    try:
-                        temp[str(i["code"])]["content"] = dict()
-                        temp[str(i["code"])]["content"][data[0]["header"][0]["value"]] = {"schema": {"$ref": None}}
-
-                        if (temp[str(i["code"])]["content"][data[0]["header"][0]["value"]]["schema"]["$ref"] is None):
-                            temp[str(i["code"])]["content"][data[0]["header"][0]["value"]]["schema"]["$ref"] = "#/referencia-de-schema-nao-definida"
-                    except:
-                        temp[str(i["code"])] = {"content": None}
-                else:
-                    temp[str(i["code"])] = None
+                temp = temp | Populate.populateResponseBody(i)
         else:
             return None
         
         return temp
+    
+
+    def createPathDescription(self, d: dict, data: dict) -> dict:
+        try:
+            d["summary"] = str(data["description"]).split(".")[0]
+        except:
+            d["summary"] = ""
+
+        try:
+            d["description"] = data["description"]
+        except:
+            d["description"] = ""
+
+        return d
 
 
     def createPaths(self, data: dict) -> dict:
         temp = dict()
-
-        for i in data["item"]:
-            for j in i["item"]:
-                path = f"/{j['request']['url']['path']}"
+        
+        for i in data:
+            for data in i["item"]:
+                path = f"/{data['request']['url']['path']}"
 
                 if (len(path) == 1):
                     path = path[0]
                 elif (len(path) > 1):
                     path = path.replace("', '", "/").replace("['", "").replace("']", "")
 
-                methods = f"{j['request']['method'].lower()}"
+                try:
+                    path = path.replace("{{", "{")
+                    path = path.replace("}}", "}")
+                except:
+                    pass
+
+                methods = f"{data['request']['method'].lower()}"
 
                 temp[path] = dict()
                 temp[path][methods] = dict()
 
                 temp[path][methods]["tags"] = [i["name"]]
 
-                try:
-                    temp[path][methods]["summary"] = str(j["request"]["description"]).split(".")[0]
-                except:
-                    temp[path][methods]["summary"] = ""
+                temp[path][methods] = self.createPathDescription(temp[path][methods], data["request"])
 
-                try:
-                    temp[path][methods]["description"] = j["request"]["description"]
-                except:
-                    temp[path][methods]["description"] = ""
-
-                temp[path][methods]["parameters"] = self.createParameters(j)
+                temp[path][methods]["parameters"] = self.createParameters(data)
 
                 # delete the parameters key if it's value is None
                 if (temp[path][methods]["parameters"]):
@@ -125,19 +139,40 @@ class Create():
                 else:
                     del temp[path][methods]["parameters"]
 
-                temp[path][methods]["requestBody"] = self.createRequestBody(j["request"], methods)
+                temp[path][methods]["requestBody"] = self.createRequestBody(data["request"], methods)
 
                 if (temp[path][methods]["requestBody"] is None):
                     del temp[path][methods]["requestBody"]
 
-                temp[path][methods]["responses"] = self.createResponseBody(j["response"], methods)
-
-
+                temp[path][methods]["responses"] = self.createResponseBody(data["response"], methods)
+        
         return temp
-    
+
 
     def createComponents(self, data: dict) -> dict:
         return 
+    
+
+    def create(self, data: dict) -> dict:
+        new_data = dict()
+
+        new_data["openapi"] = "3.0.3"
+
+        if ("info" in data.keys()):
+            new_data["info"] = self.createInfo(data["info"])
+
+        if ("variable" in data.keys()):
+            new_data["servers"] = self.createServers(data["variable"])
+
+            if (new_data["servers"] == []):
+                del new_data["servers"]
+        
+        if ("item" in data.keys()):
+            new_data["tags"] = self.createTags(data["item"])
+            new_data["paths"] = self.createPaths(data["item"])
+            new_data["components"] = self.createComponents(data["item"])
+
+        return new_data
 
 
 if __name__ == "__main__":
